38d619bdc9d8746d8662ec92826e29fb882fcae8
IceNikki <90444416+IceNikki@users.noreply.github.com>
2023-03-24 14:54:54 +0100

    Working version 1
    - Color scheme added
    - No obvious bug
    - Annual and Quarterly options

--- statsjooks/src/pages/Home.js
+++ statsjooks/src/pages/Home.js
@@ -11,10 +11,6 @@ import Contactform from '../components/Contact-form';
 import Stat from '../components/Stats';
 
 
-// Images
-// import imgF1 from '../imgs/fitness1.jpg';
-
-
 
 // fonction de rendu html
 export default function Home() {
@@ -26,13 +22,7 @@ export default function Home() {
       <div className="mt-5 container">
         <div className="row justify-content-md-center">
 
-          {/* Texte d'introduction */}
-          <div className="col-11 col-md-11 col-sm-11 col-lg-11">
-          <h2 className='title'>Please login to access the statistics of your routes on the Jooks app !</h2>        
-            <p className="mt-3 mb-4 txt-intro">
-              Every three months, we will upload the statistics of your routes on this platform, so you can access them at any time,
-            </p> 
-          </div>
+          
 
           {/* Appel du composant gérant le test ainsi que la construction du graphique de résultat */}
           <div className="col-11 col-md-11 col-sm-12 col-lg-11 mt-5 mb-2 box-quizz">
@@ -45,10 +35,10 @@ export default function Home() {
             {/* <img src={imgF1} className= "space-right" width="390" height="200" alt="" /> */}
           </div>
           
-          {/* Appel du composant du formulaire de contact */}
+          {/* Appel du composant du formulaire de contact
           <div className="col col-lg-11 mt-5 mb-5">
             <Contactform />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
